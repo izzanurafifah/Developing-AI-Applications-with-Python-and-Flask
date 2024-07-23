ad72095859ec09f99d345d92be4292276f9e4eba
Izza Nur Afifah <156738190+izzanurafifah@users.noreply.github.com>
2024-07-23 09:49:46 +0700

    Create sample1.py

---
+++ Application Development and Packaging using Python/Static Code Analysis/sample1.py
@@ -0,0 +1,16 @@
+"""
+This module contains a function to add two numbers and print the result.
+"""
+
+def add(number1, number2):
+  """
+  Returns the sum of two numbers.
+  """
+  return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+
+TOTAL = add(NUM1, NUM2)
+
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
