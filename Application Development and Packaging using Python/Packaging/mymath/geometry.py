7963f0fd240f402800cdca741ea5197f5bcea4a7
Izza Nur Afifah <156738190+izzanurafifah@users.noreply.github.com>
2024-07-23 22:59:52 +0700

    Create geometry.py

---
+++ Application Development and Packaging using Python/Packaging/mymath/geometry.py
@@ -0,0 +1,11 @@
+def area_of_rectangle(length, breadth):
+    """
+    This function returns the area of rectangle of given numbers
+    """
+    return length * breadth
+
+def area_of_circle(radius):
+    """
+    This function returns the area of circle of a given number
+    """
+    return 3.14 * (radius ** 2)
